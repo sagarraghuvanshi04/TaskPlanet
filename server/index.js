e5567e5b4eb964888e048e4a090d8619b405d234
sagarraghuvanshi04 <viplavnagde04@gmail.com>
2026-06-02 10:56:21 +0530

    fix: update render URL and cors

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: '*', credentials: true }));
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
